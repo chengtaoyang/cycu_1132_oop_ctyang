38d05448371b2b9511f53a569f0181ec050b5a64
Cheng-Tao Yang <40634259+chengtaoyang@users.noreply.github.com>
2025-02-25 19:25:52 +0800

    dev

---
+++ 20250227/Exercise-2.11.3.py
@@ -0,0 +1,11 @@
+import math
+#part 1
+r = 5
+volume = math.pow(5,3) * math.pi *4 /3
+print ('part1 volume=', volume)
+
+#part 2
+x = 42 
+v =  math.sin(x) **2 + math.cos(x) **2
+
+print ('part2 =' ,v)
