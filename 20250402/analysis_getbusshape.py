99531dd37a2fc064d538c7dcc81d6145f63277a0
Cheng-Tao Yang <40634259+chengtaoyang@users.noreply.github.com>
2025-04-03 14:35:55 +0800

    Remove obsolete Excel and GPKG files; update file paths for bus shape data and map outputs

--- 20250402/analysis_getbusshape.py
+++ 20250402/analysis_getbusshape.py
@@ -6,7 +6,7 @@ import json
 # 嘗試將內容讀取為 JSON 並轉換為 GeoDataFrame
 try:
     # 讀取 JSON 檔案
-    with open("GetBusShape", "r", encoding="utf-8") as file:
+    with open("data/GetBusShape", "r", encoding="utf-8") as file:
         content = file.read()
 
     # 先將字串內容處理為 JSON 格式
@@ -22,10 +22,10 @@ try:
     gdf = gpd.GeoDataFrame(df, geometry="geometry", crs="EPSG:4326")
     
     # 儲存為 GeoJSON 檔案
-    gdf.to_file("bus_route.geojson", driver="GeoJSON")
+    gdf.to_file("data/taipei_city_bus_route.geojson", driver="GeoJSON")
 
     # 儲存為 gpkg 檔案
-    gdf.to_file("taipei_city_bus_route.gpkg", driver="GPKG")
+    gdf.to_file("data/taipei_city_bus_route.gpkg", driver="GPKG")
 
     import folium
 
@@ -41,7 +41,7 @@ try:
         folium.PolyLine(locations=points, tooltip=row['UniRouteId']).add_to(m)
 
     # Save to HTML
-    m.save("bus_routes_map.html")
+    m.save("data/teipei_city_bus_routes_map.html")
 
 except Exception as e:
 
